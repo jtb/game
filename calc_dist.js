b0c3001c8b80e6bbc7fb5bd918409a5201585cb3
Justin Brown <brownjustin@brownjustin-macbookpro.roam.corp.google.com>
2017-07-08 21:14:40 -0700

    Single List

--- calc_dist.js
+++ calc_dist.js
@@ -3,16 +3,18 @@ var correctOrder={1:0x1,2:0x2,3:0x3,4:0x4,5:0x5,6:0x6,7:0x7,8:0x8,9:0x9,10:0xa,1
 
 function myFunction() {
     var x = document.getElementById('alist');
-    var y = document.getElementById('alist2');
+    var y = document.getElementById('wisdom');
+    //var y = document.getElementById('alist2');
     if (x.style.display === 'none') {
         x.style.display = 'block';
-	y.style.display = 'block';
+        y.style.display = 'none';
+	//y.style.display = 'block';
 	
 	document.getElementById("buttonid").innerHTML = "Score it!";
     } else {
         x.style.display = 'none';
-        y.style.display = 'none';
-	document.getElementById("buttonid").innerHTML = "Try again!";
+        y.style.display = 'block';
+	//document.getElementById("buttonid").innerHTML = "Try again!";
     }
 }
 
@@ -24,10 +26,10 @@ function calcScore() {
             newNums[parseInt($(li).text(), 10)] = idx + 1;
 	});
 
-    var listItems2 = $("#sortable2 li").not(".unsortable");
-    listItems2.each(function(idx, li) {
-            newNums[parseInt($(li).text(), 10)] = idx + 1 + listItems.length;
-	});
+    //var listItems2 = $("#sortable2 li").not(".unsortable");
+    //listItems2.each(function(idx, li) {
+    //        newNums[parseInt($(li).text(), 10)] = idx + 1 + listItems.length;
+    //});
 
     inversions = 0;
     for (i = 1; i <= 20; i++) {
@@ -45,7 +47,7 @@ function calcScore() {
     document.getElementById("demo").innerHTML = score + "%";
 
     if (score >= 90) {
-      document.getElementById("wisdom").innerHTML = "Wow, you are amazing!";
+      document.getElementById("wisdom").value = "Wow, you are amazing!";
     } else if (score >= 80) {
       document.getElementById("wisdom").innerHTML = "Great job! You know us well";
     } else if (score >= 70) {
